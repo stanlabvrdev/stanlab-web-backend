f453ee83bc0ba19b73ffed6f89ccac13352aa7e5
Oluwaleye VIctor <oluwasquarelsv@gmail.com>
2023-01-21 19:04:27 +0100

    minor change

--- controllers/labSetupController.js
+++ controllers/labSetupController.js
@@ -68,16 +68,11 @@ async function getActiveExperiments(req, res) {
         // const experiments = await LabSetup.find({ _id: { $in: experimentIds } })
         //     .select("-students -__v")
         //     .populate({ path: "teacher", select: ["name", "_id", "email"] });
-        const experiments = await LabExperiment.find({ student: req.student._id }).populate({
+        const experiments = await LabExperiment.find({ student: req.student._id, isCompleted: false }).populate({
             path: "experiment",
             select: ["name", "_id", "subject"],
         });
 
-        const student = await Student.findOne({ _id: req.student._id });
-
-        if (!student) return res.status(403).send({ message: "Access Denied" });
-
-        if (!experiments) return res.status(404).send({ message: "Not Found" });
         res.send(experiments);
     } catch (error) {
         ServerErrorHandler(req, res, error);
