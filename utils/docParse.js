d24288db6c1e6b1b29b862603ff868e687357f98
Tommy <tommyubongabasijohn@gmail.com>
2023-02-15 07:21:02 +0100

    function to format resp based on word limit

--- utils/docParse.js
+++ utils/docParse.js
@@ -51,6 +51,21 @@ async function parseDocx(fileType, buffer) {
     });
 }
 
+const splitTo500 = (text) => {
+    //Split incoming text into an array
+    const wordArr = text.split(' ')
+    let chunkSize = 500
+    //Split the array into individual arrays of 500 words each except the last portion which may not be up to 500 words
+    const arrOf500WordsEach = wordArr.reduce((acc, val, index) => {
+        if (index % chunkSize === 0) {
+            acc.push(wordArr.slice(index, (index + chunkSize)))
+        }
+        return acc;
+    }, [])
+    let output = arrOf500WordsEach.map(each => each.join(' '))
+    return output
+}
+
 
 module.exports = {
     parsePDF,
