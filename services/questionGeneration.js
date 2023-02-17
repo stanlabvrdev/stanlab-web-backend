3be5d7fc6bf9e4602f13c4dfb391de3452e2b0a2
Tommy <tommyubongabasijohn@gmail.com>
2023-02-18 00:12:32 +0100

    error handling in services

--- services/questionGeneration.js
+++ services/questionGeneration.js
@@ -4,6 +4,7 @@ const {
     parseDocx,
     splitTo500
 } = require('../utils/docParse');
+const CustomError = require('../services/exceptions/custom')
 
 //Parse pdf or text and make call to ML model
 async function genQuestions(fileType, buffer) {
@@ -13,33 +14,28 @@ async function genQuestions(fileType, buffer) {
             data = await parsePDF(buffer);
         } else if (fileType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
             data = await parseDocx(fileType, buffer)
-        } else {
-            console.log('File Type not supported')
-            //Throw an error that file type is not supported
-        }
+        } else throw CustomError(400, 'File format not supported')
+
         const formattedData = splitTo500(data.content[0])
-        console.log(formattedData)
 
         const callsToModel = formattedData.map((each) => axios.post('https://questiongen-tqzv2kz3qq-uc.a.run.app/getquestion', {
             context: each,
             option_set: "Wordnet" //Can be other or Wordnet
         }))
         //Resolve promises and extract questions
-        console.log(callsToModel)
         const questions = (await Promise.allSettled(callsToModel)).filter(each => each.status === 'fulfilled').map(each => each.value.data)
-        console.log(questions.length)
         return questions
     } catch (err) {
-        console.log(err) //Proper error handling will be carried out
+        // console.log(err.message || null)
+        throw err
     }
 }
 
 
-async function formatQuestions(arrOfQuestions) {
-    //Typescript would be helpful here, lol
+function formatQuestions(arrOfQuestions) {
+    //Typescript would be helpful here - to validte the structure of the incoming object
     let finalQuestions = []
     arrOfQuestions.forEach((each) => {
-        // console.log(each)
         for (let [question, options] of Object.entries(each)) {
             options = options.map((each) => {
                 if (each.startsWith("Ans:")) {
@@ -60,7 +56,6 @@ async function formatQuestions(arrOfQuestions) {
             })
         }
     })
-    console.log(finalQuestions)
     return finalQuestions
 }
 
