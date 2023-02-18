32da481efb9ee6b43ff89c84352fe89fd04d1aac
Tommy <tommyubongabasijohn@gmail.com>
2023-02-18 01:11:27 +0100

    question generation features routes

---
+++ routes/V2/generated-questions.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+const {
+    genFromFile,
+    genFromText,
+    saveQuestions,
+    getQuestions,
+    deleteQuestionGroup
+} = require('../../controllers/V2/question-gen.controller')
+
+const {
+    teacherAuth
+} = require("../../middleware/auth");
+
+const upload = multer({
+    fileFilter: (req, file, cb) => {
+        if (!file) {
+            return cb(new Error('No file uploaded'));
+        }
+        const allowedMimeTypes = ["application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
+        if (!allowedMimeTypes.includes(file.mimetype)) {
+            return cb(new Error("Please upload a PDF or docx file."));
+        }
+        cb(null, true);
+    },
+});
+
+router.use(teacherAuth)
+
+router.post('/filegenerate', upload.single('pdfFile'), genFromFile)
+router.post('/textgenerate', genFromText)
+router.route('/').post(saveQuestions).delete(deleteQuestionGroup).get(getQuestions)
+
+module.exports = router
