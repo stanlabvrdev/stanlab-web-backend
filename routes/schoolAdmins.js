a8898b1f011c8ab9a01a336315c85675413a880f
Kingbims <moremiabimbolamorakinyo@gmail.com>
2023-03-06 09:17:50 +0100

    update based on PR reveiw

--- routes/schoolAdmins.js
+++ routes/schoolAdmins.js
@@ -8,20 +8,19 @@ const {
   getSchoolAdmin,
   getStudents,
 } = require("../controllers/schoolAdmin.controller");
-const schoolAdminController = require("../controllers/schoolAdminsController");
 const { schoolAuth } = require("../middleware/auth");
 const { uploadFile } = require("../middleware/fileUpload");
 
 router.post("/", createSchoolAdmin);
 router.post("/teachers", schoolAuth, createTeacher);
 router.post("/students", schoolAuth, createStudent);
-router.post("/students/bulk", schoolAuth, uploadFile("student-file"), bulkCreateStudents);
+router.post(
+  "/students/bulk",
+  schoolAuth,
+  uploadFile("student-file"),
+  bulkCreateStudents
+);
 router.get("/", schoolAuth, getSchoolAdmin);
 router.get("/students", schoolAuth, getStudents);
-//router.post("/", schoolAdminController.createSchoolAdmin);
-//router.get("/", schoolAuth, schoolAdminController.getSchoolAdmin);
-//router.post("/teachers", schoolAuth, schoolAdminController.createTeacher);
-//router.post("/students", schoolAuth, schoolAdminController.createStudent);
-//router.get("/teachers", schoolAuth, schoolAdminController.getTeachers);
 
 module.exports = router;
