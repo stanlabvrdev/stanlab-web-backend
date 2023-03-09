19b31e60c6b144bceb9847e29da038d4c696406f
Oluwaleye VIctor <oluwasquarelsv@gmail.com>
2023-03-09 18:49:20 +0100

    clean ups

--- utils/db.js
+++ utils/db.js
@@ -7,4 +7,6 @@ const env = envConfig.getAll();
 module.exports = mongoose.connect(env.mongodb_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
