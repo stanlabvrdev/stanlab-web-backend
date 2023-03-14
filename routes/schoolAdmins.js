64f65ffc648b28bd105b4106bd88cc2b5903cca6
Kingbims <moremiabimbolamorakinyo@gmail.com>
2023-03-14 17:51:26 +0100

    returned response body on some endpoints and created bulk create teacher endpoint

--- routes/schoolAdmins.js
+++ routes/schoolAdmins.js
@@ -5,14 +5,17 @@ const {
   createTeacher,
   createStudent,
   bulkCreateStudents,
+  bulkCreateTeachers,
   getSchoolAdmin,
   getStudents,
+  getTeachers,
   createClass,
   addTeacherToClass,
   addStudentToClass,
   getStudentsByClass,
   getTeacherClasses,
   getClasses,
+  getClassById,
   updateClass,
 } = require("../controllers/schoolAdmin.controller");
 const { schoolAuth } = require("../middleware/auth");
@@ -27,14 +30,22 @@ router.post(
   uploadFile("student-file"),
   bulkCreateStudents
 );
+router.post(
+  "/teachers/bulk",
+  schoolAuth,
+  uploadFile("teacher-file"),
+  bulkCreateTeachers
+);
 router.get("/", schoolAuth, getSchoolAdmin);
 router.get("/students", schoolAuth, getStudents);
+router.get("/teachers", schoolAuth, getTeachers);
 router.post("/classes", schoolAuth, createClass);
-router.put("/classes/:classId/:teacherId", schoolAuth, addTeacherToClass);
-router.put("/classes/:classId/student/:studentId", schoolAuth, addStudentToClass);
+router.put("/classes/:classId/teacher", schoolAuth, addTeacherToClass);
+router.put("/classes/:classId/student", schoolAuth, addStudentToClass);
 router.get("/classes/student/:classId", schoolAuth, getStudentsByClass);
 router.get("/classes/teacher/:classId", schoolAuth, getTeacherClasses);
 router.get("/classes", schoolAuth, getClasses);
+router.get("/classes/:classId", schoolAuth, getClassById);
 router.put("/classes/:classId", schoolAuth, updateClass);
 
 module.exports = router;
