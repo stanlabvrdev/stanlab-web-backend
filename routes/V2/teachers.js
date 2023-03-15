6d993fa2952a9d5d6aee4300ac5863080cdc2c23
Kingbims <moremiabimbolamorakinyo@gmail.com>
2023-03-15 17:39:21 +0100

    profile model and endpoint for teacher to get all their school classes

--- routes/V2/teachers.js
+++ routes/V2/teachers.js
@@ -47,6 +47,9 @@ router.post("/create-class", teacherAuth, teachersController.createClass);
 
 router.get("/classes", teacherAuth, teachersController.getClass);
 
+// get school classes
+router.get("/school/classes", teacherAuth, teachersController.getSchoolClass);
+
 // post: Teacher avatar
 
 const upload = multer({
