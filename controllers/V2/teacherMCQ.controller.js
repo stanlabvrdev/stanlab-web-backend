9fa725252889cf003ad0842351f871b777fdcea5
Tommy <tommyubongabasijohn@gmail.com>
2023-03-18 15:29:36 +0100

    cleanup and updates

--- controllers/V2/teacherMCQ.controller.js
+++ controllers/V2/teacherMCQ.controller.js
@@ -5,26 +5,13 @@ const {
     ServerErrorHandler
 } = require("../../services/response/serverResponse");
 const NotFoundError = require('../../services/exceptions/not-found')
-const BadRequestError = require('../../services/exceptions/bad-request');
 
-function updateUtility(assignment, updates) {
+function updateAssignment(assignment, updates) {
     assignment.startDate = updates.startDate
     assignment.dueDate = updates.dueDate
     assignment.instruction = updates.instruction
     assignment.type = updates.type
 }
-async function getAssignmentsByClass(req, res) {
-    try {} catch (err) {
-        ServerErrorHandler(req, res, err)
-    }
-}
-
-async function getAssignment(req, res) {
-    try {} catch (err) {
-        ServerErrorHandler(req, res, err)
-    }
-}
-
 async function editAssignment(req, res) {
     try {
         const {
@@ -39,15 +26,14 @@ async function editAssignment(req, res) {
             teacherAssignment: assignment._id
         })
         const savePromises = studentAssignments.map((eachAssignment) => {
-            updateUtility(eachAssignment, req.body)
+            updateAssignment(eachAssignment, req.body)
             return eachAssignment.save()
         })
-        updateUtility(assignment, req.body)
+        updateAssignment(assignment, req.body)
         savePromises.push(assignment.save())
         await Promise.all(savePromises)
         ServerResponse(req, res, 200, assignment, 'Assignment updated successfully')
     } catch (err) {
-        console.log(err)
         ServerErrorHandler(req, res, err)
     }
 }
@@ -70,16 +56,11 @@ async function deleteAssignment(req, res) {
         await Promise.all(deletePromises)
         ServerResponse(req, res, 200, null, 'Assignment Deleted successfully')
     } catch (err) {
-        console.log(err)
         ServerErrorHandler(req, res, err)
     }
 }
 
 module.exports = {
-    getAssignment,
-    getAssignmentsByClass,
     editAssignment,
     deleteAssignment
 }
-
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2ZmNjgwMjdhZjEzNzE0MDg4NmQ0NzkiLCJyb2xlIjoiVGVhY2hlciIsImlhdCI6MTY3OTE0NzQyOX0.urX9W7egLoLTBtfUdzr8Fni_iDjYeWmY3ttv6-hczP4
