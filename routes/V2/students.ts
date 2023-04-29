3378103032a9feaa1874d50d3bc1c3d426b2a7ac
Tommy <tommyubongabasijohn@gmail.com>
2023-04-29 14:29:30 +0100

    added route for getting all assignments

--- routes/V2/students.ts
+++ routes/V2/students.ts
@@ -9,12 +9,10 @@ import studentsController from "../../controllers/V2/studentController";
 import { studentMCQController } from "../../controllers/V2/studentMCQ.controller";
 
 const router = express.Router();
-
 router.get("/labs", studentAuth, studentsController.getLabs);
 router.get("/classes", studentAuth, studentsController.getClasses);
 router.get("/classes/:classId/teachers", studentAuth, studentsController.getTeachers);
 router.get("/classes/:classId/scores", studentAuth, studentsController.getScores);
 router.get("/mcq-assignments", studentAuth, studentMCQController.getAssignments);
 router.route("/mcq-assignments/:id").get(studentAuth, studentMCQController.getAssignment).post(studentAuth, studentMCQController.makeSubmission);
-
 export default router;
