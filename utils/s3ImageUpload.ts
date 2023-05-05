38936b21c46b7efe36f8cc86d9378d06f0c63501
Tommy <tommyubongabasijohn@gmail.com>
2023-05-05 06:45:17 +0100

    utility function for image upload to s3 bucket

---
+++ utils/s3ImageUpload.ts
@@ -0,0 +1,26 @@
+import S3 from "aws-sdk/clients/s3";
+import ENV from "../config/env";
+const env = ENV.getAll();
+import CustomError from "../services/exceptions/custom";
+import { MulterFile } from "multer";
+
+export async function uploadImageToS3(file: MulterFile) {
+  const s3 = new S3({
+    accessKeyId: env.aws_access_key_id,
+    secretAccessKey: env.aws_s3_secret,
+  });
+  // Set up the S3 upload parameters
+  const params = {
+    Bucket: env.aws_bucket!,
+    Key: file.originalname,
+    Body: file.buffer,
+    ACL: "public-read",
+  };
+  try {
+    //upload the file to s3
+    const data = await s3.upload(params).promise();
+    return data.Location;
+  } catch (err) {
+    throw new CustomError(500, "Operation not successful");
+  }
+}
