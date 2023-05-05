4d9341ce0cb9d90184b1291b2db5c1e38a8ddec4
Tommy <tommyubongabasijohn@gmail.com>
2023-05-05 06:45:49 +0100

    added image upload route

--- routes/V2/generated-questions.ts
+++ routes/V2/generated-questions.ts
@@ -7,27 +7,45 @@ import { teacherAuth } from "../../middleware/auth";
 
 const router = express.Router();
 //File filter to check for file type
-function fileFilter(req, file, cb) {
+function pdfFileFilter(req, file, cb) {
   const allowedMimeTypes = ["application/pdf", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"];
   if (!allowedMimeTypes.includes(file.mimetype)) {
     return cb(new Error("Please upload a PDF or docx file."));
   }
   cb(null, true);
 }
-const upload = multer({
+
+const imageFileFilter = (req, file, cb) => {
+  // Check file type
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Invalid file type. Only JPEG, JPG, and PNG image files are allowed.");
+    return cb(error, false);
+  }
+
+  // If file type is allowed, continue with upload
+  cb(null, true);
+};
+const uploadPdf = multer({
   limits: {
     fileSize: 5000000,
   },
-  fileFilter: fileFilter,
+  fileFilter: pdfFileFilter,
 });
 
+const uploadImage = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter: imageFileFilter,
+});
 router.use(teacherAuth);
 
-router.post("/file-generate", upload.single("pdfFile"), QuestionGeneratorController.genFromFile);
+router.post("/file-generate", uploadPdf.single("pdfFile"), QuestionGeneratorController.genFromFile);
 router.post("/text-generate", QuestionGeneratorController.genFromText);
 router.post("/assign-now", QuestionGeneratorController.assignNow);
 router.post("/assign-later", QuestionGeneratorController.assignLater);
 router.route("/").post(QuestionGeneratorController.saveQuestions).get(QuestionGeneratorController.getQuestions);
 router.route("/:id").delete(QuestionGeneratorController.deleteQuestionGroup).get(QuestionGeneratorController.getAQuestion).put(QuestionGeneratorController.editAQuestionGroup);
-
+router.put("/:questionID/image", uploadImage.single("image"), QuestionGeneratorController.addImage);
 export default router;
