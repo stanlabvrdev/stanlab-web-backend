ab53a668f1dfd0dba29feb0b5c9446638a1dc48b
Oluwaleye VIctor <oluwasquarelsv@gmail.com>
2023-05-13 20:10:45 +0100

    [IMP] added interface to config

--- config/env.ts
+++ config/env.ts
@@ -2,8 +2,37 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+interface IEnv {
+  environment: string;
+  port: number;
+
+  app_name: string;
+  jwtKey: string;
+  sendGrid_API_KEY: string;
+  teacher_google_CLIENT_ID: string;
+  teacher_google_CLIENT_SECRET: string;
+  student_google_CLIENT_ID: string;
+  student_google_CLIENT_SECRET: string;
+  mongodb_URI: string;
+  stripe_Publishable_Key: string;
+  stripe_Secret_Key: string;
+  cloudinary_api_key: string;
+  cloudinary_secret_key: string;
+  mailgun_API_KEY: string;
+  aws_bucket: string;
+  aws_access_key_id: string;
+  aws_s3_secret: string;
+  question_generation_model: string;
+  paystack_payment_URL: string;
+  paystack_secret_key: string;
+  paystack_verification_URL: string;
+  paystack_recurring_payment_URL: string;
+  redirect_URL: string;
+  free_subscription_title: string;
+}
+
 class Env {
-  getAll() {
+  getAll(): IEnv {
     const development = "development";
     const env = process.env.NODE_ENV || development;
 
@@ -15,34 +44,34 @@ class Env {
 
     return this.getDefault();
   }
-  getDefault() {
+  getDefault(): IEnv {
     return {
-      environment: process.env.NODE_ENV,
-      port: Number(process.env.PORT),
+      environment: process.env.NODE_ENV!,
+      port: Number(process.env.PORT)!,
 
-      app_name: process.env.APP_NAME,
-      jwtKey: process.env.JWT_KEY,
-      sendGrid_API_KEY: process.env.SENDGRID_API_KEY,
-      teacher_google_CLIENT_ID: process.env.TEACHER_GOOGLE_CLIENT_ID,
-      teacher_google_CLIENT_SECRET: process.env.TEACHER_GOOGLE_CLIENT_SECRET,
-      student_google_CLIENT_ID: process.env.STUDENT_GOOGLE_CLIENT_ID,
-      student_google_CLIENT_SECRET: process.env.STUDENT_GOODLE_CLIENT_SECRET,
-      mongodb_URI: process.env.MONGODB_URL,
-      stripe_Publishable_Key: process.env.STRIPE_PUBLIC_KEY,
-      stripe_Secret_Key: process.env.STRIPE_SECRET_KEY,
-      cloudinary_api_key: process.env.CLOUDINARY_PUBLIC_KEY,
-      cloudinary_secret_key: process.env.CLOUDINARY_SECRET_KEY,
-      mailgun_API_KEY: process.env.MAILGUN_API_KEY,
-      aws_bucket: process.env.AWS_BUCKET,
-      aws_access_key_id: process.env.AWS_S3_ACCESS_KEY,
-      aws_s3_secret: process.env.AWS_S3_SECRET,
-      question_generation_model: process.env.QUESTION_GENERATION_MODEL,
-      paystack_payment_URL: process.env.PAYSTACK_PAYMENT_URL,
-      paystack_secret_key: process.env.PAYSTACK_SECRET_KEY,
-      paystack_verification_URL: process.env.PAYSTACK_VERIFICATION_URL,
-      paystack_recurring_payment_URL: process.env.PAYSTACK_RECURRING_PAYMENT_URL,
-      redirect_URL: process.env.REDIRECT_URL,
-      free_subscription_title: process.env.FREE_SUBSCRIPTION_TITLE,
+      app_name: process.env.APP_NAME!,
+      jwtKey: process.env.JWT_KEY!,
+      sendGrid_API_KEY: process.env.SENDGRID_API_KEY!,
+      teacher_google_CLIENT_ID: process.env.TEACHER_GOOGLE_CLIENT_ID!,
+      teacher_google_CLIENT_SECRET: process.env.TEACHER_GOOGLE_CLIENT_SECRET!,
+      student_google_CLIENT_ID: process.env.STUDENT_GOOGLE_CLIENT_ID!,
+      student_google_CLIENT_SECRET: process.env.STUDENT_GOODLE_CLIENT_SECRET!,
+      mongodb_URI: process.env.MONGODB_URL!,
+      stripe_Publishable_Key: process.env.STRIPE_PUBLIC_KEY!,
+      stripe_Secret_Key: process.env.STRIPE_SECRET_KEY!,
+      cloudinary_api_key: process.env.CLOUDINARY_PUBLIC_KEY!,
+      cloudinary_secret_key: process.env.CLOUDINARY_SECRET_KEY!,
+      mailgun_API_KEY: process.env.MAILGUN_API_KEY!,
+      aws_bucket: process.env.AWS_BUCKET!,
+      aws_access_key_id: process.env.AWS_S3_ACCESS_KEY!,
+      aws_s3_secret: process.env.AWS_S3_SECRET!,
+      question_generation_model: process.env.QUESTION_GENERATION_MODEL!,
+      paystack_payment_URL: process.env.PAYSTACK_PAYMENT_URL!,
+      paystack_secret_key: process.env.PAYSTACK_SECRET_KEY!,
+      paystack_verification_URL: process.env.PAYSTACK_VERIFICATION_URL!,
+      paystack_recurring_payment_URL: process.env.PAYSTACK_RECURRING_PAYMENT_URL!,
+      redirect_URL: process.env.REDIRECT_URL!,
+      free_subscription_title: process.env.FREE_SUBSCRIPTION_TITLE!,
     };
   }
   getProduction() {
@@ -51,8 +80,8 @@ class Env {
   getDevelopment() {
     return { ...this.getDefault() };
   }
-  getTest() {
-    return { ...this.getDefault(), jwtKey: "test-key" };
+  getTest(): IEnv {
+    return { ...this.getDefault(), jwtKey: "test-key", mailgun_API_KEY: "MG.test", sendGrid_API_KEY: "SG.test" };
   }
 }
 
