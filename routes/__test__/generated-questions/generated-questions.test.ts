5e33252f8b6077a598a96fbf92de7c18df97d512
Tommy <tommyubongabasijohn@gmail.com>
2023-05-21 14:18:10 +0100

    more tests

--- routes/__test__/generated-questions/generated-questions.test.ts
+++ routes/__test__/generated-questions/generated-questions.test.ts
@@ -8,60 +8,114 @@ import { createQuestionGroup } from "../../../test/topical-questions";
 const baseURL = global.baseURL;
 
 const url = `${baseURL}/v2/ai/questions/`;
-it("can only be accessed if teacher is signed in", async () => {
-  await request(app).get(url).send({}).expect(401);
-});
 
-const performSaveOperation = async (type: string) => {
-  let savedQuestions: any;
-  const teacher = await global.loginTeacher();
-  if (type === "MCQ") {
-    savedQuestions = await GeneratedQuestions.insertMany(fakeMCQQuestions, { rawResult: false });
-  } else if (type === "TOF") {
-    savedQuestions = await GeneratedQuestions.insertMany(fakeTOFQuestions, { rawResult: false });
-  }
+describe("Question Group ENDPOINTS", () => {
+  describe("SAVE /v2/ai/questions/", () => {
+    it("can only be accessed if teacher is signed in", async () => {
+      await request(app).get(url).send({}).expect(401);
+    });
 
-  const res = await request(app).post(url).set("x-auth-token", teacher.token).send({
-    questions: savedQuestions,
-    subject: "Biology",
-    topic: "Pollination",
+    const performSaveOperation = async (type: string) => {
+      let savedQuestions: any;
+      const teacher = await global.loginTeacher();
+      if (type === "MCQ") {
+        savedQuestions = await GeneratedQuestions.insertMany(fakeMCQQuestions, { rawResult: false });
+      } else if (type === "TOF") {
+        savedQuestions = await GeneratedQuestions.insertMany(fakeTOFQuestions, { rawResult: false });
+      }
+
+      const res = await request(app).post(url).set("x-auth-token", teacher.token).send({
+        questions: savedQuestions,
+        subject: "Biology",
+        topic: "Pollination",
+      });
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("message", "Questions saved to 'Question bank'");
+
+      const data = res.body.data;
+      expect(data).toHaveProperty("questions");
+      expect(data.questions).toBeInstanceOf(Array);
+      expect(data.questions.length).toBeGreaterThan(0); // Ensure questions array is not empty
+      expect(data).toHaveProperty("_id");
+      expect(data).toHaveProperty("teacher");
+      expect(data).toHaveProperty("subject");
+      expect(data).toHaveProperty("topic");
+    };
+
+    it("should save MCQ questions with valid payload", async () => {
+      await performSaveOperation("MCQ");
+    });
+
+    it("should save TOF questions with valid payload", async () => {
+      await performSaveOperation("TOF");
+    });
   });
 
-  expect(res.status).toBe(200);
-  expect(res.body).toHaveProperty("message", "Questions saved to 'Question bank'");
+  describe("GET /v2/ai/questions/", () => {
+    it("should get question groups for the logged in teacher", async () => {
+      const teacher = await global.loginTeacher();
+      await createQuestionGroup(teacher._id);
+      const res = await request(app).get(url).query({ teacher: teacher._id }).set("x-auth-token", teacher.token);
 
-  const data = res.body.data;
-  expect(data).toHaveProperty("questions");
-  expect(data.questions).toBeInstanceOf(Array);
-  expect(data.questions.length).toBeGreaterThan(0); // Ensure questions array is not empty
-  expect(data).toHaveProperty("_id");
-  expect(data).toHaveProperty("teacher");
-  expect(data).toHaveProperty("subject");
-  expect(data).toHaveProperty("topic");
-};
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe("Successful");
+      expect(res.body.data[0].teacher).toEqual(teacher._id.toString());
+    });
 
-it("should save MCQ questions with valid payload", async () => {
-  await performSaveOperation("MCQ");
-});
+    it("should return message if teacher has no saved questions", async () => {
+      const teacher = await global.loginTeacher();
+      const res = await request(app).get(url).query({ teacher: teacher._id }).set("x-auth-token", teacher.token);
 
-it("should save TOF questions with valid payload", async () => {
-  await performSaveOperation("TOF");
-});
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe("You have no saved questions");
+    });
+  });
 
-it("should get question groups for the logged in teacher", async () => {
-  const teacher = await global.loginTeacher();
-  await createQuestionGroup(teacher._id);
-  const res = await request(app).get(url).query({ teacher: teacher._id }).set("x-auth-token", teacher.token);
+  describe("DELETE /v2/ai/questions/:id", () => {
+    it("should delete a question group if it was created by the logged in teacher", async () => {
+      const teacher = await global.loginTeacher();
+      const questionGroup = await createQuestionGroup(teacher._id);
+      const id = questionGroup._id;
+      const newUrl = `${url}/${id}`;
+      const res = await request(app).delete(newUrl).set("x-auth-token", teacher.token);
 
-  expect(res.status).toBe(200);
-  expect(res.body.message).toBe("Successful");
-  expect(res.body.data[0].teacher).toEqual(teacher._id.toString());
-});
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe("Deleted Successfully");
+      expect(res.body.data).toBe(null);
+    });
 
-it("should return message if teacher has no saved questions", async () => {
-  const teacher = await global.loginTeacher();
-  const res = await request(app).get(url).query({ teacher: teacher._id }).set("x-auth-token", teacher.token);
+    it("should return a message if the resource is not found", async () => {
+      const teacher = await global.loginTeacher();
+      const newUrl = `${url}/60aae530b4fb6a001f4e93cc`;
+      const res = await request(app).delete(newUrl).set("x-auth-token", teacher.token);
 
-  expect(res.status).toBe(404);
-  expect(res.body.message).toBe("You have no saved questions");
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe("Resource not found");
+      expect(res.body.data).toBe(null);
+    });
+  });
+
+  describe("GET /v2/ai/questions/:id", () => {
+    it("should get questions of a question group", async () => {
+      const teacher = await global.loginTeacher();
+      const questionGroup = await createQuestionGroup(teacher._id);
+      const id = questionGroup._id;
+      const newUrl = `${url}/${id}`;
+
+      const res = await request(app).get(newUrl).set("x-auth-token", teacher.token);
+      console.log(res.body);
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe("Successful");
+      expect(res.body.data.questions).toBeInstanceOf(Array);
+      expect(res.body.data.teacher).toEqual(teacher._id.toString());
+    });
+    it("should return a message if the resource is not found", async () => {
+      const teacher = await global.loginTeacher();
+      const newUrl = `${url}/60aae530b4fb6a001f4e93cc`;
+      const res = await request(app).get(newUrl).set("x-auth-token", teacher.token);
+      expect(res.status).toBe(404);
+      expect(res.body.message).toBe("Questions, Not found");
+    });
+  });
 });
