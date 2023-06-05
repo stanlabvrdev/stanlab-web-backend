1747f55643f24c2cca172f4e43324c5817342398
Kingbims <moremiabimbolamorakinyo@gmail.com>
2023-06-05 11:26:40 +0100

    added title and removed school email

--- controllers/schoolAdmin.controller.ts
+++ controllers/schoolAdmin.controller.ts
@@ -24,6 +24,7 @@ export const createSchoolAdmin = async (req, res) => {
       .header("access-control-expose-headers", "x-auth-token")
       .send({
         admin_name: data.admin.adminName,
+        admin_title: data.admin.adminTitle,
         email: data.admin.email,
         schoolEmail: data.admin.schoolEmail,
         schoolName: data.admin.schoolName,
@@ -42,10 +43,7 @@ export const createTeacher = async (req, res) => {
     const { error } = validateSchoolUser(req.body);
     if (error) throw new BadRequestError(error.details[0].message);
 
-    await schoolAdminService.createTeacher(
-      req.body,
-      req.school._id
-    );
+    await schoolAdminService.createTeacher(req.body, req.school._id);
     ServerResponse(req, res, 201, null, "invitation sent sucessfully");
   } catch (error) {
     ServerErrorHandler(req, res, error);
@@ -81,10 +79,7 @@ export const bulkCreateStudents = async (req, res) => {
 
 export const bulkCreateTeachers = async (req, res) => {
   try {
-    await schoolAdminService.bulkCreateTeachers(
-      req,
-      req.school._id
-    );
+    await schoolAdminService.bulkCreateTeachers(req, req.school._id);
     ServerResponse(req, res, 201, null, "teachers added sucessfully");
   } catch (error) {
     ServerErrorHandler(req, res, error);
