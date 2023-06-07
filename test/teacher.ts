0510f0589745a04e6d9b499daab09b8e1561046f
Tommy <tommyubongabasijohn@gmail.com>
2023-06-07 09:52:29 +0100

    removed unused function and added complete test cases

--- test/teacher.ts
+++ test/teacher.ts
@@ -12,13 +12,3 @@ export async function createClass(teacherId: string) {
 
   return teacherClass.save();
 }
-
-export async function addStudentToClass(classId: string, name: string) {
-  const student = await createStudent(name);
-
-  const studentClass = new StudentTeacherClass({
-    student: student._id,
-    class: classId,
-  });
-  await studentClass.save();
-}
