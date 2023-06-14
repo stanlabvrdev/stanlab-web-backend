b04093def729322200c80ebface98572bb2b7507
Kingbims <moremiabimbolamorakinyo@gmail.com>
2023-06-14 10:01:30 +0100

    test fro remove student

--- routes/__test__/schoolAdmin/students.test.ts
+++ routes/__test__/schoolAdmin/students.test.ts
@@ -6,9 +6,12 @@ import { StudentSubscription } from "../../../models/student-subscription";
 
 const baseURL = global.baseURL;
 
-const url = `${baseURL}/schools/classes/:classId/student`;
+const url = `${baseURL}/schools`;
 it("can only be accessed if admin is signed in", async () => {
-  await request(app).put(url).send({}).expect(401);
+  await request(app)
+    .put(`${url}/classes/:classId/student`)
+    .send({})
+    .expect(401);
 });
 
 it("should create a student and add them to the class", async () => {
@@ -19,7 +22,7 @@ it("should create a student and add them to the class", async () => {
   await addStudentToClass(school._id, teacherClass._id, name);
 
   const res = await request(app)
-    .put(`${baseURL}/schools/classes/${teacherClass._id}/student`)
+    .put(`${url}/classes/${teacherClass._id}/student`)
     .set("x-auth-token", school.token)
     .send({
       name,
@@ -39,4 +42,22 @@ it("should create a student and add them to the class", async () => {
   expect(schoolStudent.school.toString()).toBe(school._id.toString());
   expect(subscribe).toBeDefined();
   expect(subscribe.school.toString()).toBe(school._id.toString());
+});
+
+it("should remove a student", async () => {
+  const school = await global.loginSchool();
+  const teacherClass = await createClass();
+  let name = "test student";
+
+  let student = await addStudentToClass(school._id, teacherClass._id, name);
+
+  const res = await request(app)
+    .delete(`${url}/remove-students`)
+    .set("x-auth-token", school.token)
+    .send({
+      studentId: [student._id],
+    });
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.data).toBe(null);
 });
