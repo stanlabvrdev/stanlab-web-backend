f86c2f8883e170b78a6c106993f218df083d6e40
Tommy <tommyubongabasijohn@gmail.com>
2023-07-16 07:02:56 +0100

    update validation

--- middleware/validate.lesson-plan.ts
+++ middleware/validate.lesson-plan.ts
@@ -1,19 +1,15 @@
-import { plainToInstance } from "class-transformer";
-import { validate, ValidationError } from "class-validator";
 import { Request, Response, NextFunction } from "express";
-import BadRequestError from "../services/exceptions/bad-request";
+import Joi from "joi";
 
 export class ValidationMiddleware {
-  static validate<T>(type: any): (req: Request, res: Response, next: NextFunction) => void {
+  static validate(schema: Joi.ObjectSchema) {
     return (req: Request, res: Response, next: NextFunction) => {
-      validate(plainToInstance(type, req.body)).then((errors: ValidationError[]) => {
-        if (errors.length > 0) {
-          const message = errors.map((error: ValidationError) => Object.values(error.constraints || {})).join(", ");
-          throw new BadRequestError(message);
-        } else {
-          next();
-        }
-      });
+      const { error } = schema.validate(req.body);
+      if (error) {
+        res.status(400).json({ message: error.details[0].message });
+      } else {
+        next();
+      }
     };
   }
 }
--- routes/V2/lesson-plan.routes.ts
+++ routes/V2/lesson-plan.routes.ts
@@ -2,15 +2,15 @@ import express from "express";
 import { teacherAuth } from "../../middleware/auth";
 import { LessonPlanController } from "../../controllers/V2/lesson-plan.controller";
 import { ValidationMiddleware } from "../../middleware/validate.lesson-plan";
-import { CreateLessonPlanDto, GenerateLessonPlanDto, UpdateLessonPlanDto } from "../../services/lesson-plan/lesson-plan.dto";
+import { CreateLessonPlanSchema, GenerateLessonPlanSchema, UpdateLessonPlanSchema } from "../../services/lesson-plan/lesson-plan.dto";
 
 const lessonPlanController = new LessonPlanController();
 const router = express.Router();
 
 router.use(teacherAuth);
 
-router.route("/:Id").get(lessonPlanController.getLessonPlan).put(ValidationMiddleware.validate(UpdateLessonPlanDto), lessonPlanController.updateLessonPlan).delete(lessonPlanController.deleteLessonPlan);
-router.route("/").get(lessonPlanController.getLessonPlans).post(ValidationMiddleware.validate(CreateLessonPlanDto), lessonPlanController.createLessonPlan);
-router.post("/generate", ValidationMiddleware.validate(GenerateLessonPlanDto), lessonPlanController.generateLessonPlan);
+router.route("/:Id").get(lessonPlanController.getLessonPlan).put(ValidationMiddleware.validate(UpdateLessonPlanSchema), lessonPlanController.updateLessonPlan).delete(lessonPlanController.deleteLessonPlan);
+router.route("/").get(lessonPlanController.getLessonPlans).post(ValidationMiddleware.validate(CreateLessonPlanSchema), lessonPlanController.createLessonPlan);
+router.post("/generate", ValidationMiddleware.validate(GenerateLessonPlanSchema), lessonPlanController.generateLessonPlan);
 
 export default router;
