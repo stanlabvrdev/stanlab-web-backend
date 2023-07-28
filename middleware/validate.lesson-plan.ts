a3dc93581752a3c33fdc3969dd52b360128736df
Oluwaleye Victor <38075323+drlsv91@users.noreply.github.com>
2023-07-28 14:54:34 +0100

    Update validate.lesson-plan.ts
    removed redundant else statement

--- middleware/validate.lesson-plan.ts
+++ middleware/validate.lesson-plan.ts
@@ -10,9 +10,9 @@ export class ValidationMiddleware {
       try {
         if (error) {
           throw new BadRequestError(error.details[0].message);
-        } else {
-          next();
         }
+          next();
+        
       } catch (err) {
         ServerErrorHandler(req, res, err);
       }
