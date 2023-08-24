8ad9b1becbcdebc45148cc5d86e613089102cb43
Tommy <tommyubongabasijohn@gmail.com>
2023-08-24 17:42:56 +0100

    access restriction

--- routes/V2/timetable.ts
+++ routes/V2/timetable.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { TimeTableController } from "../../controllers/V2/timetable.controller";
 import { ValidationMiddleware } from "../../middleware/validate.lesson-plan";
 import { scheduleSchema } from "../../services/timetable/timetable.dto";
+import { schoolAuth } from "../../middleware/auth";
 
 const router = express.Router();
 
-router.route("/").post(ValidationMiddleware.validate(scheduleSchema), TimeTableController.create);
+router
+  .route("/")
+  .post(schoolAuth, ValidationMiddleware.validate(scheduleSchema), TimeTableController.create);
 
 export { router as timetableRoute };
