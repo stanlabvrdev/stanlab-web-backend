0c3fd1af5403099d8cf698da45cfde16f15ad1f1
Tommy <tommyubongabasijohn@gmail.com>
2023-08-24 17:09:55 +0100

    time-table generation

---
+++ routes/V2/timetable.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { TimeTableController } from "../../controllers/V2/timetable.controller";
+import { ValidationMiddleware } from "../../middleware/validate.lesson-plan";
+import { scheduleSchema } from "../../services/timetable/timetable.dto";
+
+const router = express.Router();
+
+router.route("/").post(ValidationMiddleware.validate(scheduleSchema), TimeTableController.create);
+
+export { router as timetableRoute };
