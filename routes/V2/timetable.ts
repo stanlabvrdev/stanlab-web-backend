ee4186ff5c97be8b699c01d271731bf22a3f8518
Tommy <tommyubongabasijohn@gmail.com>
2023-08-24 17:56:00 +0100

    test fix

--- routes/V2/timetable.ts
+++ routes/V2/timetable.ts
@@ -6,6 +6,8 @@ import { schoolAuth } from "../../middleware/auth";
 
 const router = express.Router();
 
-router.route("/").post(ValidationMiddleware.validate(scheduleSchema), TimeTableController.create);
+router
+  .route("/")
+  .post(schoolAuth, ValidationMiddleware.validate(scheduleSchema), TimeTableController.create);
 
 export { router as timetableRoute };
